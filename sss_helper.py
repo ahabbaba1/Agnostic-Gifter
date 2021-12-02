0b24bf757ec5d5b3faa10144929401770e0e7331
unknown <aysha.habbaba@gmail.com>
2021-12-02 03:25:22 -0800

    email funcs added

--- sss_helper.py
+++ sss_helper.py
@@ -7,8 +7,7 @@
 # Date: 12/1
 ##################
 
-import csv
-import random
+import csv, random, smtplib, constants
 
 # read in + parse CSV
 def read_csv(filename):
@@ -24,27 +23,34 @@ def read_csv(filename):
 # assign folks 
 def assign_giftees(peeps):
 	gifted = []
-	possibleGiftees = peeps[:]
+	possible_giftees = peeps[:]
 	for p, peep in enumerate(peeps):
 		# remove current gifter as a possible giftee
-		if (len(possibleGiftees) != 1 and possibleGiftees[0][1] == peep[1]): del possibleGiftees[0]
+		if (len(possible_giftees) != 1 and possible_giftees[0][1] == peep[1]): del possible_giftees[0]
 
 		# randomly pick a giftee from pool
-		rand = random.randint(0, len(possibleGiftees) - 1)	
-		print(peep[1], "gives to ", possibleGiftees[rand][1])
+		rand = random.randint(0, len(possible_giftees) - 1)	
+		# print(peep[1], "gives to ", possible_giftees[rand][1])
 		
 		# remove giftee from giftee pool
-		gifted.append(possibleGiftees[rand])
-		del possibleGiftees[rand]
+		gifted.append(possible_giftees[rand])
+		del possible_giftees[rand]
 
 		# add current gifter back to giftee pool if they haven't already gotten a gift
 		# (ik, horrid complexity)
-		if (not(peep in gifted)):  possibleGiftees.append(peep)
+		if (not(peep in gifted)): possible_giftees.append(peep)
 
+def setup_mail():
+	s = smtplib.SMTP(constants.SMTP_SERVER, constants.SMTP_PORT)
+	s.starttls()
+	s.login(constants.SENDER_EMAIL, constants.SENDER_PASSWORD)
 
-# send email
+def send_mail(smtp_session, receiver, message):
+	smtp_session.sendmail(constants.SENDER_EMAIL, receiver, message)
 
-
+def close_smtp_session(smtp_session):
+	smtp_session.quit()
+	
 # bb driver (icu ansel)
-names = read_csv("strings.csv")
+names = read_csv(constants.FILENAME)
 assign_giftees(names)
